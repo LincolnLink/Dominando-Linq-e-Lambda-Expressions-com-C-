8ea7e46bcf7a2d750709c3ac95dcc682945cdef0
Lincoln Ferreira Campos <link7-7@hotmail.com>
2018-11-30 19:23:33 -0200

    #Tutorial 3

---
+++ DominandoLinQ/Loja/Program.cs
@@ -0,0 +1,55 @@
+﻿using LojaDominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loja
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var Produto = new Produto(); /*instanciando uma classe*/
+            var Produtos = Produto.Listar(); /* Usando um método, e botando o resultado em uma variavel*/
+
+
+            /*Pode filtrar direito com Where*/
+            var Produto2 = new Produto(); /*instanciando uma classe*/
+            var Produtos2 = Produto2.Listar().Where(p => p.Quantidade > 4); /*R: Banana e Melão!*/
+
+
+            /*Retorna somente os nomes de cada item!*/
+            foreach (Produto item in Produtos)
+            {
+                Console.WriteLine(item.Nome);
+            }
+
+
+            /*Forech em uma unica linha!*/
+            Console.WriteLine("\n Usando uma linha: ");
+
+            new Produto().Listar().ForEach(item => Console.WriteLine(item.Nome)); /*R: todos os filtros*/
+
+
+
+            /*Forech exibindo mais valores, filtrados e convertendo para lista!*/
+            Console.WriteLine("\n Usando uma linha com filtro: ");
+
+            new Produto().Listar().Where(item => item.Valor > 5).ToList().ForEach(item =>
+            {
+                Console.WriteLine(item.Nome);
+                Console.WriteLine(item.Valor); /*R: Morango25, Pera12*/
+            });
+
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ DominandoLinQ/LojaDominio/Produto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaDominio
+{
+    public class Produto
+    {
+        /*private ICollection<Produto> _produto3;*/
+        /*private IEnumerable<Produto> _produto2;*/
+        private List<Produto> _produtos;
+
+        /*crt+R+G : limpa os using que não estão sendo usados*/
+
+        public Guid Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public int Quantidade { get; set; }
+
+        public decimal Valor { get; set; }
+
+        public DateTime DataDeVencimento { get; set; }
+
+
+        public Produto()
+        {
+            _produtos = new List<Produto>();
+        }
+
+        public List<Produto> Listar()
+        {
+            _produtos.Add(new Produto() { Id = Guid.NewGuid(), Nome = "Banana", Quantidade = 5, Valor = 2, DataDeVencimento = DateTime.Now.AddDays(5) });
+            _produtos.Add(new Produto() { Id = Guid.NewGuid(), Nome = "Morango", Quantidade = 4, Valor = 25, DataDeVencimento = DateTime.Now.AddDays(2) });
+            _produtos.Add(new Produto() { Id = Guid.NewGuid(), Nome = "Pera", Quantidade = 2, Valor = 12, DataDeVencimento = DateTime.Now.AddDays(3) });
+            _produtos.Add(new Produto() { Id = Guid.NewGuid(), Nome = "Abacaxi", Quantidade = 1, Valor = 4, DataDeVencimento = DateTime.Now.AddDays(4) });
+            _produtos.Add(new Produto() { Id = Guid.NewGuid(), Nome = "Melão", Quantidade = 35, Valor = 5, DataDeVencimento = DateTime.Now.AddDays(10) });
+
+            return _produtos;
+        }
+
+    }
+}
