2665239e21fb72ad7a2776f963742c769cbf7fe6
Lincoln Ferreira Campos <link7-7@hotmail.com>
2018-12-01 02:49:27 -0200

    #Tutorial 4

--- DominandoLinQ/Loja/Program.cs
+++ DominandoLinQ/Loja/Program.cs
@@ -1,4 +1,5 @@
 ﻿using LojaDominio;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,8 @@ namespace Loja
         {
             var Produto = new Produto(); /*instanciando uma classe*/
             var Produtos = Produto.Listar(); /* Usando um método, e botando o resultado em uma variavel*/
+
+            #region Turotial-3 
 
 
             /*Pode filtrar direito com Where*/
@@ -45,11 +48,81 @@ namespace Loja
             });
 
 
+            #endregion
 
+
+            #region Tutorial-4
+
+            var ProdutosTutorial4 = new Produto().Listar(); /*Instnacia*/
+
+            /* StratsWith: indica um valor que inicia com o caracter informado no parametro */
+            /* EndsWith: indica um valor que finaliza com o caracter informado no parametro */
+            Console.WriteLine("\n Json: \n");
+
+            ProdutosTutorial4 = ProdutosTutorial4.Where(p => p.Nome.StartsWith("A") || p.Nome.EndsWith("o")).ToList();
+
+            ProdutosTutorial4.ForEach(item =>
+            {
+                Console.WriteLine(JsonConvert.SerializeObject(item)); /*R: Morango, Abacaxi e Melão*/
+                Console.WriteLine(" ");
+
+            });
+
+            /* Objeto Anônimo pelo .Select */
+            Console.WriteLine("\n Criando Objeto Anônimo: \n");
+
+            var ProdutoT4 = new Produto().Listar();
+            var ProdutosString = ProdutoT4
+                .Where(p => p.Nome.StartsWith("A") || p.Nome.EndsWith("o"))
+                .Select(p => new
+                    {
+                        Nome = p.Nome,
+                        Valor = p.Valor
+                    }
+                )
+                .ToList();
+
+            ProdutosString.ForEach(p => Console.WriteLine(p)); /*R: Morango, Abacaxi e Melão*/
+
+
+            /* Objeto Tipado pelo .Select() */
+            /* Exemplo do .SubString(0,4): exibe somente os caracter do index 1° até o 4°*/
+            /* Exemplo do .Insert(4, "texto"): inseri o texto depois do 4°index*/
+            /* Exemplo do .Remove(IndexInicial, IndexFinal): remove de acordo com o index informado*/
+            /* Exemplo do .Replace("a", "Z") : subistitui tudo que tem "a" por "Z" */
+            Console.WriteLine("\n Criando Objeto Tipado, exemplo do .SubString() e .Insert() : \n");
+
+            var ProdutoT42 = new Produto().Listar();
+            var ProdutoStringType = ProdutoT42
+                .Where(p => p.Nome.StartsWith("A") || p.Nome.EndsWith("o"))
+                .Select(p => new ItensSelecionados()
+                    {
+                        Nome = p.Nome,
+                        Valor = p.Valor,
+                        Dia = p.DataDeVencimento.Day
+                    }
+                )
+                .ToList();
+
+            ProdutoStringType.ForEach(p => Console.WriteLine("Nome: "+ p.Nome.Substring(0,4).Insert(4," -Abreviado") +"/ Valor: "+ p.Valor +"/ Dia: "+ p.Dia)); /*R: Morango, Abacaxi e Melão*/
+
+            Console.WriteLine("\n .Remove() e .Replace(): \n");
+            ProdutoStringType.ForEach(p => Console.WriteLine(p.Nome.Remove(0, 1).Replace("a", "#"), p.Valor));
+
+            #endregion
 
 
             Console.ReadKey();
 
         }
+    }
+
+    public class ItensSelecionados
+    {
+        public string Nome { get; set; }
+
+        public decimal Valor { get; set; }
+
+        public int Dia { get; set; }
     }
 }
